a7a8abc812f0d59a02902786b35fa2f2a3bc83f2
Muhammad Ahmad Ahsan <ahmadahsan7172@gmail.com>
2026-05-22 20:19:03 +0500

    Add logger.py

---
+++ app/logger.py
@@ -0,0 +1,14 @@
+import logging
+from config import LOGS_PATH
+
+def setup_logging():
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        handlers=[
+            logging.FileHandler(LOGS_PATH)
+        ]
+    )
+
+def get_logger(name):
+    return logging.getLogger(name)
